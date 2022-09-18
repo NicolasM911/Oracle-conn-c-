b0f030cab8b8b2194cf9f0bea5c0b09516ef185e
NicolasM <nicolasmahecha11@hotmail.com>
2022-09-18 17:25:28 -0500

    first commit

---
+++ Login2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Login2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTestCon = new System.Windows.Forms.Button();
+            this.lblConn = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblCerrado = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTestCon
+            // 
+            this.btnTestCon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTestCon.Location = new System.Drawing.Point(609, 319);
+            this.btnTestCon.Name = "btnTestCon";
+            this.btnTestCon.Size = new System.Drawing.Size(149, 40);
+            this.btnTestCon.TabIndex = 0;
+            this.btnTestCon.Text = "Test Conn";
+            this.btnTestCon.UseVisualStyleBackColor = true;
+            this.btnTestCon.Click += new System.EventHandler(this.btnTestCon_Click);
+            // 
+            // lblConn
+            // 
+            this.lblConn.AutoSize = true;
+            this.lblConn.Location = new System.Drawing.Point(29, 346);
+            this.lblConn.Name = "lblConn";
+            this.lblConn.Size = new System.Drawing.Size(90, 13);
+            this.lblConn.TabIndex = 2;
+            this.lblConn.Text = "Estado Conexion:";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblVersion.Location = new System.Drawing.Point(29, 319);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(45, 13);
+            this.lblVersion.TabIndex = 3;
+            this.lblVersion.Text = "Version:";
+            // 
+            // lblCerrado
+            // 
+            this.lblCerrado.AutoSize = true;
+            this.lblCerrado.Location = new System.Drawing.Point(29, 377);
+            this.lblCerrado.Name = "lblCerrado";
+            this.lblCerrado.Size = new System.Drawing.Size(47, 13);
+            this.lblCerrado.TabIndex = 4;
+            this.lblCerrado.Text = "Cerrado:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Location = new System.Drawing.Point(609, 365);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(149, 25);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(32, 294);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(726, 2);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Location = new System.Drawing.Point(233, 208);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(308, 45);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(231, 49);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtUser.Size = new System.Drawing.Size(310, 37);
+            this.txtUser.TabIndex = 8;
+            this.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(233, 125);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtPass.Size = new System.Drawing.Size(310, 37);
+            this.txtPass.TabIndex = 9;
+            this.txtPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 442);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblCerrado);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblConn);
+            this.Controls.Add(this.btnTestCon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestCon;
+        private System.Windows.Forms.Label lblConn;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblCerrado;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLogin;
+        public System.Windows.Forms.TextBox txtUser;
+        public System.Windows.Forms.TextBox txtPass;
+    }
+}
+
---
+++ Login2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+//using Oracle.ManagedDataAccess.Client;
+
+namespace Login2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private OracleConnection conexion = new OracleConnection("DATA SOURCE = xe ; PASSWORD = 123 ; USER ID = testlogin ");
+        private void btnTestCon_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                lblVersion.Text = $"La Version del Servidor es {conexion.ServerVersion}";
+                lblConn.Text += $"la Conexion esta: ";
+                lblConn.Text += conexion.State.ToString();
+                lblConn.Text += "\nSe Accede a la base de datos";
+
+                MessageBox.Show(@"
+                Conexión Establecida Correctamente
+                Conectado a la Base de Datos");
+                lblVersion.Text = $"La Version del Servidor es {conexion.ServerVersion}";
+                conexion.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Hay un Error al acceder a Su Base de Datos {ex.Message}");
+                lblConn.Text = $"Hay un Error al acceder a Su Base de Datos {ex.Message}";
+                
+            }
+            finally
+            {
+                conexion.Close();
+                lblCerrado.Text += $" - Ahora la Conexion esta: {conexion.State.ToString()}";
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            OracleCommand comando = new OracleCommand($"SELECT * FROM PERSONAS WHERE NOM_USER = :user AND PASS_USER = :pass", conexion);
+            comando.Parameters.AddWithValue(":user", txtUser.Text);
+            comando.Parameters.AddWithValue(":pass", txtPass.Text);
+
+            OracleDataReader lector = comando.ExecuteReader();
+
+            if (lector.Read())
+            {
+                MessageBox.Show("Conexion exitosa a oracle");
+                Formulario formulario = new Formulario();
+                conexion.Close();
+                formulario.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usario y contraseña incorrectos");
+            }
+            conexion.Close();
+        }
+    }
+}
